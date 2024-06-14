e19dab1cfed72112eff3a04597af328200dc265d
Nyasaa <nyasasharma08@gmail.com>
2024-06-14 19:23:24 +0530

    add chnanges

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Report from '../../Components/Report/Report';
+
+const Home = () => {
+  return (
+    <div className='w-screen h-screen flex justify-center items-center sm:border-b-2 sm:border-white'>
+        hello
+        <Report/>
+    </div>
+  )
+}
+
+export default Home
