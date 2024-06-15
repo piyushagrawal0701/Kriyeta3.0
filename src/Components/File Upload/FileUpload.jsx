a454c08be0a4428c03e0ccb44b3d9bee590aee67
Piyush-Agrawal7 <agrawal777piyush@gmail.com>
2024-06-15 11:43:42 +0530

    Dashboard page updated

--- src/Components/File Upload/FileUpload.jsx
+++ src/Components/File Upload/FileUpload.jsx
@@ -33,7 +33,9 @@ const FileUpload = () => {
     <div>
       <form onSubmit={handleSubmit}>
         <input type="file" accept=".csv" onChange={handleFileChange} required />
-        <button type="submit">Upload</button>
+        <button class="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">
+              Upload
+            </button>
       </form>
     </div>
   );
