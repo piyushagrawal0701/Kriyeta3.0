dba99a84cb590fd18549ad6d87a3011b28154277
Piyush-Agrawal7 <agrawal777piyush@gmail.com>
2024-06-16 03:56:42 +0530

    Netlify toml added

--- src/Components/File Upload/FileUpload.jsx
+++ src/Components/File Upload/FileUpload.jsx
@@ -1,42 +1,42 @@
 import React, { useState } from "react";
 
 const FileUpload = () => {
-  const [file, setFile] = useState(null);
+  const [success, setSuccess] = useState("");
 
-  const handleFileChange = (e) => {
-    setFile(e.target.files[0]);
-  };
-
-  const handleSubmit = async (e) => {
+  const handleFileUpload = async (e) => {
     e.preventDefault();
     const formData = new FormData();
+    const file = e.target.elements.file.files[0];
+
     formData.append("file", file);
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/upload", {
+      const response = await fetch("/process", {
         method: "POST",
         body: formData,
       });
 
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
+      if (response.ok) {
+        setSuccess("Data has been successfully stored in Firebase.");
+      } else {
+        setSuccess("There was an error processing your file.");
       }
-
-      const result = await response.json();
-      alert(result.message);
     } catch (error) {
-      console.error("Error:", error);
+      setSuccess("There was an error processing your file.");
     }
   };
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input type="file" accept=".csv" onChange={handleFileChange} required />
+      <form onSubmit={handleFileUpload} encType="multipart/form-data">
+        <label htmlFor="file">Choose a file:</label>
+        <input type="file" name="file" id="file" required />
         <button class="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">
           Upload
         </button>
       </form>
+
+      {success && <p>{success}</p>}
     </div>
   );
 };
